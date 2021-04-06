48e3b0d85327685a8551c5123ee13e9861cc129d
Christerle Nounoute <christerlesn@gmail.com>
2021-04-06 23:10:36 +0000

    Done.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,15 +13,19 @@ Rails.application.configure do
   config.eager_load = false
 
   # Configure static file server for tests with Cache-Control for performance.
-  config.serve_static_files   = true
-  config.static_cache_control = 'public, max-age=3600'
 
+  config.serve_static_files   = true
+  # config.public_file_server.enabled = true
+
+  config.static_cache_control = 'public, max-age=3600'
+  # config.public_file_server.headers = { 'Cache-Control' => 'public, max-
+# age=3600' }
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
