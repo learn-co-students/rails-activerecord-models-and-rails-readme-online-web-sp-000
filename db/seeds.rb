84df6431e5004b493506612e04dc117d1ca8945f
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-04-06 12:21:30 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+describe Post do
+    it 'can be created' do
+      post = Post.create!(title: "My title", description: "The post description")
+      expect(post).to be_valid
+    end
+  end
+  
